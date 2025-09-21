c528a8b7f60a049deb11d4e4a0fee79a0c6e8900
Mikhail <misha.konopkin@gmail.com>
2025-09-21 03:21:47 +0300

    авторизация в контейнерах

---
+++ recognition_backend/authapi/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token  # для токенов (опционально)
+from django.contrib.auth import login, logout
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+
+class UserRegistrationView(APIView):
+    permission_classes = []  # разрешаем всем
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # Создаём токен (если хочешь токенную аутентификацию)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                "user_id": user.id,
+                "username": user.username,
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            login(request, user)  # создаёт сессию
+
+            # Генерируем/получаем токен
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({
+                "message": "Успешный вход",
+                "user_id": user.id,
+                "username": user.username,
+                "token": token.key
+            }, status=status.HTTP_200_OK)
+        return Response({"error": "Неверный логин или пароль"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Выход выполнен"}, status=status.HTTP_200_OK)
