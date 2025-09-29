4d586277af8703966964e9b2ced2c0aa8e800c6d
Mikhail <misha.konopkin@gmail.com>
2025-09-29 11:14:31 +0300

    add callback

--- app/image_api/urls.py
+++ app/image_api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from . import views
+from .callbacks import image_location_callback
 from .views import UploadImageView, GetUserImageLocationsView
 
 urlpatterns = [
     path('upload-images/', UploadImageView.as_view(), name='upload_images'),
     path('user/image-locations/', GetUserImageLocationsView.as_view(), name='user-image-locations'),
+    path('callback/', image_location_callback, name='image-location-callback'),
     # path('upload/', views.upload_image, name='upload_image'),
 ]
 
