d78dfcd103a61d989ead6b2833c414b1db814243
Misha <misha.konopkin@gmail.com>
2025-10-02 09:54:13 +0300

    hotfixes

--- app/image_api/utils.py
+++ app/image_api/utils.py
@@ -20,8 +20,8 @@ def _send_geo_request_internal(images):
                 'raw_response': original response dict (optional)
             }
     """
-    callback_url = f"{settings.API_BASE_URL}:80/api/update-image-result/"
-    url = f"{settings.EXTERNAL_SERVICE_URL}:5000/api/Prediction"
+    callback_url = f"{settings.API_BASE_URL}:8000/api/update-image-result/"
+    url = f"{settings.EXTERNAL_SERVICE_URL}:8080/api/Prediction"
 
     tasks = []
     for img in images:
